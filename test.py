ef7392aca0f2460a7bb45b03730f83c2c7b6817c
Shili Cao <92201647+Shirou2021@users.noreply.github.com>
2022-05-26 22:37:26 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+import analyzer
+from unittest import main
+
+# Test your function by calling it here
+analyzer.data_analysis()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
